abab9ffbad8044cad646ba336533c8b302c93c5a
SESPX <78691846+SESPX@users.noreply.github.com>
2023-11-19 14:53:23 +0100

    Domasna ciklusi

---
+++ Domasna ciklusi/script.js
@@ -0,0 +1,12 @@
+// Ciklus za pecatenje na sekoj vtor neparen broj od 1 do 100
+for (let i = 1; i <= 100; i += 4) {
+    if (i % 2 !== 0) {
+      console.log(i);
+    }
+  }
+
+// Cikls za pecatenej na sekoj neparen broj od 1 do 50
+for (let i = 1; i <= 50; i += 2) {
+    console.log(i);
+  }
+  
